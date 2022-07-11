3119c4acc3e158952265a08611af2ac8736f33b4
Mr. Pine <50425705+Mr-Pine@users.noreply.github.com>
2022-07-11 22:16:37 +0200

    Dependency Updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-val composeVersion = "1.2.0-beta03"
-val lifecycleVersion = "2.5.0-rc01"
+val composeVersion = "1.3.0-alpha01"
+val lifecycleVersion = "2.6.0-alpha01"
 
 android {
     compileSdk = 32
@@ -49,20 +49,20 @@ android {
 }
 
 dependencies {
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha02")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha03")
 
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
-    implementation("androidx.compose.material3:material3:1.0.0-alpha13")
+    implementation("androidx.compose.material3:material3:1.0.0-alpha14")
 
     implementation("com.google.android.material:material:1.7.0-alpha02")
 
-    implementation("androidx.navigation:navigation-compose:2.5.0-rc01")
+    implementation("androidx.navigation:navigation-compose:2.5.0")
 
-    implementation("androidx.core:core-ktx:1.9.0-alpha04")
+    implementation("androidx.core:core-ktx:1.9.0-alpha05")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
-    implementation("androidx.activity:activity-compose:1.6.0-alpha03")
+    implementation("androidx.activity:activity-compose:1.6.0-alpha05")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
 
     debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
