377366945837ee440636751fbd81b1b309196685
MisterPine <git@mr-pine.de>
2023-09-06 17:50:53 +0200

    Building with android

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("multiplatform")
     id("org.jetbrains.compose")
-    //id("com.android.library")
+    id("com.android.library")
     kotlin("plugin.serialization") version "1.9.10"
 }
 
@@ -9,8 +9,7 @@ group = "de.mr_pine.recipes"
 version = "1.0-SNAPSHOT"
 
 kotlin {
-    //androidTarget()
-    jvmToolchain(17)
+    androidTarget()
     jvm("desktop")
 
     @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
@@ -29,39 +28,30 @@ kotlin {
                 implementation(kotlin("test"))
             }
         }
-        /*val androidMain by getting {
+        val androidMain by getting {
             dependencies {
                 api("androidx.appcompat:appcompat:1.6.1")
                 api("androidx.core:core-ktx:1.10.1")
-                //api("androidx.compose.runtime:runtime:1.5.0")
-                //api("androidx.compose.ui:ui:1.5.0")
-                //api("androidx.compose.material3:material3:1.2.0-alpha06")
+                api("androidx.compose.runtime:runtime:1.5.0")
+                api("androidx.compose.ui:ui:1.5.0")
+                api("androidx.compose.material3:material3:1.2.0-alpha06")
             }
         }
-        val androidUnitTest by getting {
-            dependencies {
-                implementation("junit:junit:4.13.2")
-            }
-        }*/
         val desktopMain by getting {
             dependencies {
                 api(compose.preview)
                 api(compose.material)
             }
         }
-        val desktopTest by getting
     }
 
 }
 
-/*android {
+android {
     compileSdk = 34
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     defaultConfig {
         minSdk = 26
-    }
-    buildFeatures {
-        compose = true
     }
     namespace = "de.mr_pine.recipes.common"
     compileOptions {
@@ -71,7 +61,7 @@ kotlin {
     kotlin {
         jvmToolchain(11)
     }
-}*/
+}
 
 task("testClasses").doLast {
     println("This is a dummy testClasses task")
---
+++ common/src/androidMain/kotlin/de/mr_pine/recipes/common/model/instructions/call.kt
@@ -0,0 +1,15 @@
+package de.mr_pine.recipes.common.model.instructions
+
+import android.content.Context
+import android.content.Intent
+import android.provider.AlarmClock
+
+fun InstructionSubmodels.TimerModel.call(title: String, context: Context) {
+    val intent = Intent(AlarmClock.ACTION_SET_TIMER).apply {
+        putExtra(AlarmClock.EXTRA_MESSAGE, title)
+        putExtra(AlarmClock.EXTRA_LENGTH, duration.inWholeSeconds.toInt())
+        putExtra(AlarmClock.EXTRA_SKIP_UI, false)
+    }
+    context.startActivity(intent)
+}
+
