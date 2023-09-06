26bb4cf2dbf624ff88ebeae9650e2617eeea0aa3
MisterPine <git@mr-pine.de>
2023-09-06 20:54:38 +0200

    SwipeToDismiss (broken)

--- android/src/main/java/de/mr_pine/recipes/android/model_views/view/InstructionViews.kt
+++ android/src/main/java/de/mr_pine/recipes/android/model_views/view/InstructionViews.kt
@@ -21,12 +21,16 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -39,7 +43,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.google.android.material.color.MaterialColors
 import de.mr_pine.recipes.android.R
-import de.mr_pine.recipes.android.components.swipeable.Swipeable
 import de.mr_pine.recipes.android.ui.theme.Extended
 import de.mr_pine.recipes.common.model.RecipeIngredient
 import de.mr_pine.recipes.common.model.instructions.InstructionSubmodels
@@ -77,16 +80,31 @@ fun RecipeInstruction.InstructionCard(
 
     val currentColor = if (done) Extended.revertOrange else Extended.doneGreen
 
+    val dismissState = rememberDismissState(
+        confirmValueChange = {
+            when (it) {
+                DismissValue.Default -> true
+                DismissValue.DismissedToEnd -> {
+                    toggleDone()
+                    false
+                }
 
-    Swipeable(
-        swipeRightComposable = { _, relative ->
+                DismissValue.DismissedToStart -> true
+            }
+        }
+    )
+
+    SwipeToDismiss(
+        state = dismissState,
+        directions = setOf(DismissDirection.StartToEnd),
+        background = {
             Card(
                 colors = CardDefaults.cardColors(
                     containerColor = Color(
                         MaterialColors.layer(
                             MaterialTheme.colorScheme.surfaceVariant.toArgb(),
                             currentColor.accentContainer.toArgb(),
-                            relative
+                            1f//dismissState.progress
                         )
                     )
                 ), modifier = Modifier.fillMaxSize()
@@ -107,75 +125,70 @@ fun RecipeInstruction.InstructionCard(
                 }
             }
         },
-        rightSwipedDone = {
-            toggleDone(); Log.d(
-            TAG,
-            "InstructionCard: done: $done, index: $index, currentIndex: $currentlyActiveIndex"
-        )
-        }
-    ) {
-        val containerColor = MaterialTheme.colorScheme.let {
-            if (done) it.surface.copy(alpha = 0.38f)
-                .compositeOver(it.surfaceColorAtElevation(1.dp)) else it.surface
-        }
-        ElevatedCard(
-            modifier = Modifier
-                .fillMaxWidth(),
-            colors = CardDefaults.elevatedCardColors(
-                containerColor = containerColor,
-                contentColor = contentColorFor(backgroundColor = containerColor).copy(alpha = if (done) 0.38f else 1f)
-            ),
-            onClick = {
-                setCurrentlyActiveIndex(index)
+        dismissContent = {
+            val containerColor = MaterialTheme.colorScheme.let {
+                if (done) it.surface.copy(alpha = 0.38f)
+                    .compositeOver(it.surfaceColorAtElevation(1.dp)) else it.surface
             }
-        ) {
+            ElevatedCard(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                colors = CardDefaults.elevatedCardColors(
+                    containerColor = containerColor,
+                    contentColor = contentColorFor(backgroundColor = containerColor).copy(alpha = if (done) 0.38f else 1f)
+                ),
+                onClick = {
+                    setCurrentlyActiveIndex(index)
+                }
+            ) {
 
-            Column(modifier = Modifier.padding(12.dp)) {
-                val context = LocalContext.current
+                Column(modifier = Modifier.padding(12.dp)) {
+                    val context = LocalContext.current
 
-                EmbeddedText(
-                    inlineEmbeds = inlineEmbeds,
-                    done = done,
-                    enabled = {enabled},
-                    embedChipOnClick = {
-                        when (it.embed) {
-                            is InstructionSubmodels.IngredientModel -> it.enabled = !it.enabled
-                            is InstructionSubmodels.TimerModel -> (it.embed as InstructionSubmodels.TimerModel).call(
-                                recipeTitle,
-                                context
-                            )
-                        }
-                    },
-                    content = content,
-                    ingredients = ingredients
-                )
-                AnimatedVisibility(
-                    visible = active,
-                    enter = scaleIn(initialScale = 0f) + expandVertically(expandFrom = Alignment.Top),
-                    exit = scaleOut(targetScale = 0f) + shrinkVertically(shrinkTowards = Alignment.Top)
-                ) {
-                    Row(
-                        horizontalArrangement = Arrangement.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 8.dp)
-                    ) {
-                        Button(
-                            onClick = ::toggleDone,
-                            colors = ButtonDefaults.buttonColors(
-                                contentColor = currentColor.onAccentContainer,
-                                containerColor = currentColor.accentContainer
-                            )
-                        ) {
-                            Text(
-                                text = if (done) stringResource(R.string.step_repeat) else stringResource(
-                                    R.string.step_finish
+                    EmbeddedText(
+                        inlineEmbeds = inlineEmbeds,
+                        done = done,
+                        enabled = { enabled },
+                        embedChipOnClick = {
+                            when (it.embed) {
+                                is InstructionSubmodels.IngredientModel -> it.enabled = !it.enabled
+                                is InstructionSubmodels.TimerModel -> (it.embed as InstructionSubmodels.TimerModel).call(
+                                    recipeTitle,
+                                    context
                                 )
-                            )
+                            }
+                        },
+                        content = content,
+                        ingredients = ingredients
+                    )
+                    AnimatedVisibility(
+                        visible = active,
+                        enter = scaleIn(initialScale = 0f) + expandVertically(expandFrom = Alignment.Top),
+                        exit = scaleOut(targetScale = 0f) + shrinkVertically(shrinkTowards = Alignment.Top)
+                    ) {
+                        Row(
+                            horizontalArrangement = Arrangement.Center,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(top = 8.dp)
+                        ) {
+                            Button(
+                                onClick = ::toggleDone,
+                                colors = ButtonDefaults.buttonColors(
+                                    contentColor = currentColor.onAccentContainer,
+                                    containerColor = currentColor.accentContainer
+                                )
+                            ) {
+                                Text(
+                                    text = if (done) stringResource(R.string.step_repeat) else stringResource(
+                                        R.string.step_finish
+                                    )
+                                )
+                            }
                         }
                     }
                 }
             }
         }
-    }
+    )
 }
